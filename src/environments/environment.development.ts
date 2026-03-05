6ed0b89148b36a7feb5d7e730f78cb9592694056
le7-3609 <240203746+le7-3609@users.noreply.github.com>
2026-03-05 10:47:18 +0200

    chore: stop tracking ignored files and folders

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  googleClientId: '80499258556-ejjm82bap1m8qddo4tormeeujms28qpa.apps.googleusercontent.com',
-  apiUrl: 'https://localhost:44371/api'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  googleClientId: '' // נשאר ריק כי זה יעלה לשרת האמיתי ושם נגדיר זאת אחרת
-};
